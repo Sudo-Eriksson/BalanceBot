faeeafb858234b85d336a2040371d42e4841ea14
johaneriksson <joerikss@student.chalmers.se>
2020-06-24 20:18:25 +0200

    Added motor speed mapping according to the lowest PWM signal. Improved motor speed verification.

--- src/main.cpp
+++ src/main.cpp
@@ -11,33 +11,43 @@
 // Define LED pin
 #define pinLED 2
 
-void set_motor_speeds(signed int speed){
+boolean set_motor_speeds(signed int speed){
   /*
   * Method for writing speed to the motors. Sets the same amount of speed to the both motors.
-  * Param: speed - Signed integer that corresponds to the wanted speed of the motor. Range of 0 - 255.
+  * Param: speed - Signed integer that corresponds to the wanted speed of the motor. Range of -100 - 100.
+  * Return: Boolean - True if the method executed successfully. False if something failed.
   */
-
-  // The motors do no go lower than this.
-  if(abs(speed) < 90){
-    return;
+  
+  // Check if the speed is whithin the allowed range.
+  if (speed < -100 || speed > 100){
+    Serial.print("Speed out of range: "); Serial.println(speed);
+    return false;
   }
 
   // Set the direction of the motors
-  if (speed > 0){
+  if (speed >= 0){
+      // Forward
       digitalWrite(pinDir1M1, HIGH);
       digitalWrite(pinDir2M1, LOW);
       digitalWrite(pinDir1M2, LOW);
       digitalWrite(pinDir2M2, HIGH);
   }
   else{
+      // Backward 
       digitalWrite(pinDir1M1, LOW);
       digitalWrite(pinDir2M1, HIGH);
       digitalWrite(pinDir1M2, HIGH);
       digitalWrite(pinDir2M2, LOW);
   }
-  // Write the speed of the motors
-  analogWrite(pinM1, abs(speed));
-  analogWrite(pinM2, abs(speed));
+
+  // Map a value between 0 and 100 to a range accepted by the motors.
+  long speed_map = map(abs(speed), 0, 100, 89, 255);
+
+  // Write the speed to the motors
+  analogWrite(pinM1, speed_map);
+  analogWrite(pinM2, speed_map);
+
+  return true;
 }
 
 void setup() {
@@ -58,20 +68,8 @@ void setup() {
 
 void loop() {
   // Example run of the motors
-  for (int i=100; i<=200; i=i+10){
+  for (int i=-120; i<= 130; i += 10){
     set_motor_speeds(i);
-    delay(500);
-  }
-  for (int i=200; i>=100; i=i-10){
-    set_motor_speeds(i);
-    delay(500);
-  }
-  for (int i=-100; i>=-200; i=i-10){
-    set_motor_speeds(i);
-    delay(500);
-  }
-  for (int i=-200; i>=-100; i=i+10){
-    set_motor_speeds(i);
-    delay(500);
+    delay(750);
   }
 }
